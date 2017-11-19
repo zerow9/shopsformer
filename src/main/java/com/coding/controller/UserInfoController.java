6f48f6c4e7f7318b60794a9c90c78dab7429b0a6
张伟 <1350379257@qq.com>
2017-11-19 15:15:11 +0800

    更换邮箱

--- src/main/java/com/coding/controller/UserInfoController.java
+++ src/main/java/com/coding/controller/UserInfoController.java
@@ -8,6 +8,7 @@ import com.coding.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.net.InetAddress;
@@ -27,16 +28,16 @@ public class UserInfoController {
     }
 
     @RequestMapping("userInfo")
-    public String first(HttpServletRequest request ) throws Exception {
-        String uuid = (String)request.getSession().getAttribute("uuid");
+    public String first(HttpServletRequest request) throws Exception {
+        String uuid = (String) request.getSession().getAttribute("uuid");
         User user = adminService.selectUserByPrimaryKey(uuid);
         user.setUserRegisterDateTimeToString(DateToString.date(user.getUserRegisterDateTime()));
-        request.setAttribute("user",user);
+        request.setAttribute("user", user);
         return "persons/information";
     }
 
     @RequestMapping("updateUser")
-    public String updateUser(User user) throws  Exception{
+    public String updateUser(User user) throws Exception {
         user.setUserGroup(2);
         user.setUserCurrentTime(new Date());
         user.setUserLandIp(InetAddress.getLocalHost().getHostAddress());
@@ -45,10 +46,10 @@ public class UserInfoController {
     }
 
     @RequestMapping("addUser")
-    public String addUser(String emailId,User user,HttpSession session) throws  Exception{
-        Integer emailCode=(Integer) session.getAttribute("emailCode");
-        String email=""+emailId;
-        if(emailCode!=null&&email.equals(emailId)) {
+    public String addUser(String emailId, User user, HttpSession session) throws Exception {
+        Integer emailCode = (Integer) session.getAttribute("emailCode");
+        String email = "" + emailId;
+        if (emailCode != null && email.equals(emailId)) {
             user.setUserUuid(MyUUID.randomUUID());
             user.setUserGroup(2);
             user.setUserRegisterDateTime(new Date());
@@ -57,19 +58,19 @@ public class UserInfoController {
             user.setUserScore(0);
             user.setUserLandIp(InetAddress.getLocalHost().getHostAddress());
             adminService.insertUserSelective(user);
-            return "homes/index";
+            return "homes/login";
         }
         return "homes/register";
     }
 
     @RequestMapping("email")
-    public void email(String email, HttpSession session){
-        Integer integer=100000+new Random().nextInt(899999);
-        MessageTools message=MessageTools.initTool();
+    public void email(String email, HttpSession session) {
+        Integer integer = 100000 + new Random().nextInt(899999);
+        MessageTools message = MessageTools.initTool();
         message.setBoxTitle("【孝和商城】验证码");
         message.setInbox(email);
-        message.setText("【孝和商城】您在孝和商城请求的验证码是："+integer);
+        message.setText("【孝和商城】您在孝和商城请求的验证码是：" + integer);
         message.sendQQMessage();
-        session.setAttribute("emailCode",integer);
+        session.setAttribute("emailCode", integer);
     }
 }
