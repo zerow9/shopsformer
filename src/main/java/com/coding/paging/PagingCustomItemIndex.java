fd92e1b4a54d29418d1c1598fd3dde11b61f7dc0
xhu <xhusoul@sina.com>
2017-11-20 13:28:43 +0800

    itemIndex 表增加基础 增删查改 功能

---
+++ src/main/java/com/coding/paging/PagingCustomItemIndex.java
@@ -0,0 +1,15 @@
+package com.coding.paging;
+
+import com.coding.pojo.ItemIndex;
+
+public class PagingCustomItemIndex extends Paging{
+    private ItemIndex itemIndex;
+
+    public ItemIndex getItemIndex() {
+        return itemIndex;
+    }
+
+    public void setItemIndex(ItemIndex itemIndex) {
+        this.itemIndex = itemIndex;
+    }
+}
