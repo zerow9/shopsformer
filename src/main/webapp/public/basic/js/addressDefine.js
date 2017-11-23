d6c25ed249eb44b9eba48e57f98094192dfb0523
zerow9 <825323237@qq.com>
2017-11-23 10:17:02 +0800

    Address验证

--- src/main/webapp/public/basic/js/addressDefine.js
+++ src/main/webapp/public/basic/js/addressDefine.js
@@ -35,3 +35,23 @@ function deleteAddress(addressId) {
     });
 
 }
+
+function phoneNumber() {    // 验证手机号码
+    var phoneNumber=document.getElementById("userPhone").value;//获取密码框的值
+    var Regex = /((1[3-5])\d{9})|((1[7-8])\d{9})|(0[1-9]{2,3}\-?[1-9]{6,7})/i;
+    if (Regex.test(phoneNumber)){
+        document.getElementById("phoneNumber").innerHTML="";
+    }
+    else {
+        if (phoneNumber == "") {
+            document.getElementById("phoneNumber").innerHTML="请输入手机号码！";//检测到密码为空，提醒输入//
+            document.getElementById("userPhone").focus();//焦点放到密码框
+            return false;
+        }
+        else {
+            document.getElementById("phoneNumber").innerHTML="手机号码格式不正确，请重新输入！";//检测到密码为空，提醒输入//
+            document.getElementById("userPhone").focus();//焦点放到密码框
+            return false;
+        }
+    }
+}
