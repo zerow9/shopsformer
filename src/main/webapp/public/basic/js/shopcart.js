e033507749e0e73445623f44bd737ab65db2fb1f
周兴兴 <1123195494@qq.com>
2017-11-27 17:36:16 +0800

    购买商品数量不能大于数据库的商品数量

--- src/main/webapp/public/basic/js/shopcart.js
+++ src/main/webapp/public/basic/js/shopcart.js
@@ -8,7 +8,7 @@ $(function () {
        // var eleli=$(this).parent().parent().parent().parent().parent().find("li.td.td-sum>.td-inner>.number").html(1);
         var number = ele.find(".text_box").val();// 获取商品的数量
         var pice=eleul.find(".price-now").html();//获取商品的价格
-
+        ele.find(".add").attr("disabled",false);
         if (number == 1) {
             ele.find(".min").attr("disabled",true);
         } else {
@@ -45,6 +45,7 @@ $(function () {
                     eleul.find(".number").html(pice*(number+1));
                     ele.find('.text_box').val(number + 1);
                 }else if(data=='error'){
+                    ele.find(".add").attr("disabled",true);
                     swal({
                         title: "商品数量不足！",
                         type: "error",
