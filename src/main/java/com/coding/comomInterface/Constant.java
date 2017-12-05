fbcd96265bb8b6436ceae8efe2468398854cb3df
张伟 <1350379257@qq.com>
2017-12-05 17:57:23 +0800

    常量变成property

--- src/main/java/com/coding/comomInterface/Constant.java
+++ src/main/java/com/coding/comomInterface/Constant.java
@@ -1,8 +1,23 @@
 package com.coding.comomInterface;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Constant {
+
+    private static Properties properties = new Properties();
+
+    static {
+        String path = "E:\\IDECode\\shopsformer\\src\\main\\resources\\constant\\constant.properties";
+        try {
+            InputStream fileInputStream = new FileInputStream(path);
+            properties.load(fileInputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static final String FaceImage = new File(".").getPath() + File.separator + "1.png";
 
@@ -17,5 +32,10 @@ public class Constant {
     public static final Integer PAGE_NUMBER = 15;
 
     public static final Integer ERROR_NUMBER = -1;
+
+
+    public static String setName(String name) {
+        return properties.getProperty(name);
+    }
 
 }
